6561e325358f34d3d9a39b2f68d34f2058d3eed6
Ini Aku Bram <88739434+PURELIGHT1@users.noreply.github.com>
2025-06-11 06:06:33 +0700

    state config

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,29 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    minify: "esbuild",
+    cssMinify: "lightningcss",
+    rollupOptions: {
+      output: {
+        generatedCode: {
+          preset: "es2015",
+          arrowFunctions: true,
+          constBindings: true,
+          objectShorthand: true,
+          reservedNamesAsProps: true,
+          symbols: false,
+        },
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 })
